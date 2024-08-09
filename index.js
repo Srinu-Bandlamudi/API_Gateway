9b403c090fc25678c13e87bdb106863a0176670b
Srinu-Bandlamudi <srinubandlamudi55@gmail.com>
2024-08-09 19:12:52 +0530

    Added Rate limit logic

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
 const express =require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const morgan=require('morgan');
+const rateLimit  = require('express-rate-limit');
 
 
 const app=express();
 
 const PORT=3006;
 
+const limiter = rateLimit({
+	windowMs: 2 * 60 * 1000, // 15 minutes
+	limit: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	
+})
+
+
 app.use(morgan('combined'));
+app.use(limiter);
+
+app.use('/bookingservice', async (req, res, next) => {
+    console.log(req.headers['x-access-token']);
+    try {
+        const response = await axios.get('http://localhost:3001/api/v1/isauthenticated', {
+            headers: {
+                'x-access-token': req.headers['x-access-token']
+            }
+        });
+        console.log(response.data);
+        if(response.data.success) {
+            next();
+        } else {
+            return res.status(401).json({
+                message: 'Unauthorised'
+            })
+        }
+    } catch (error) {
+        return res.status(401).json({
+            message: 'Unauthorised'
+        })
+    }
+})
+
 app.use(
     '/bookingservice',
     createProxyMiddleware({
